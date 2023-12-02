b79c9679ba290633067c2900300c41ab05a35522
Yan Wen <z19040042@s.upc.edu.cn>
2023-12-02 22:46:07 +0800

    Complete with basic shooting features including autofocus, folder incrementing, picture serial number incrementing, saving both jpg and dng and shooting UI.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 开源项目
 https://www.theverge.com/21306907/diy-camera-raspberry-pi-high-quality-how-to-build-video-c-cs-mount-lenses
+
+
+# TODO
--- camera_pi.py
+++ camera_pi.py
@@ -14,15 +14,54 @@ import numpy as np
 import time
 import configparser
 import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+# Set up logging
+log_file_path = os.path.expanduser("~/camera_app.log")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        RotatingFileHandler(log_file_path, maxBytes=1024*1024, backupCount=5),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
 
 config_file_path = os.path.expanduser("~/.camera_config")
 config = configparser.ConfigParser()
 
+
 try:
     config.read(config_file_path)
     total_captures = config.getint("General", "total_captures")
+    output_folder_num=config.getint("General","output_folder_num")
 except (configparser.NoSectionError, configparser.NoOptionError):
     total_captures = 0
+    output_folder_num=100
+
+output_folder_path=os.path.expanduser(f"~/图片/{output_folder_num}_DCIM")
+os.makedirs(output_folder_path, exist_ok=True)
+# output_folder = os.path.expanduser("~/图片/100_DCIM")
+# current_folder = 100
+
+
+# def increment_folder():
+#     global current_folder
+#     current_folder += 1
+#     config["General"]["current_folder"] = str(current_folder)
+#
+# def get_next_folder():
+#
+#     if total_captures > 9999:
+#         increment_folder()
+#         return os.path.join(os.path.expanduser(f"~/图片/{current_folder}_DCIM"))
+#     else:
+#         return output_folder
 
 
 
@@ -58,19 +97,39 @@ overlay_black[:, :] = (0, 0, 0, 255)
 
 def on_button_clicked():
     global total_captures
+    global output_folder_num
+    global output_folder_path
+
     circular_button.setEnabled(False)
 
+    # output_folder_path = get_next_folder()
+    # os.makedirs(output_folder_path, exist_ok=True)
 
     qpicamera2.set_overlay(overlay_black)
     cfg = picam2.create_still_configuration(raw={})
     total_captures += 1
-    picam2.switch_mode_and_capture_file(cfg, f"DSC_{total_captures:04d}.jpg", signal_function=qpicamera2.signal_done)
-    picam2.switch_mode_and_capture_file(cfg, f"DSC_{total_captures:04d}.dng", name="raw",signal_function=qpicamera2.signal_done)
+
+    if total_captures>9999:
+        total_captures=1
+        output_folder_num+=1
+        output_folder_path=os.path.expanduser(f"~/图片/{output_folder_num}_DCIM")
+        os.makedirs(output_folder_path, exist_ok=True)
+
+
+    jpg_file=os.path.join(output_folder_path,f"DSC_{total_captures:04d}.jpg")
+    dng_file = os.path.join(output_folder_path, f"DSC_{total_captures:04d}.dng")
+    # picam2.switch_mode_and_capture_file(cfg,jpg_file , signal_function=qpicamera2.signal_done)
+
+    # TODO: When an object is moving and the image is captured this way, the dng file and the jpg file will not be exactly the same.
+    picam2.switch_mode_and_capture_file(cfg, dng_file, name="raw",signal_function=qpicamera2.signal_done)
+    picam2.switch_mode_and_capture_file(cfg, jpg_file, signal_function=qpicamera2.signal_done)
     # time.sleep(0.05)
     # overlay[:, :] = (0, 0, 0, 0)
     # qpicamera2.set_overlay(overlay)
 
-    config["General"] = {"total_captures": str(total_captures)}
+    config["General"] = {"total_captures": str(total_captures),
+                         "output_folder_num": str(output_folder_num)}
+    # config["General"] = {"output_folder_num": str(output_folder_num)}
     with open(config_file_path, "w") as configfile:
         config.write(configfile)
 
